a48769642f121fa3ae9bfc3074bfa10700d46c90
kpk-abs <120717722+kpk-abs@users.noreply.github.com>
2023-03-13 11:30:43 +0530

    Test sandbox

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import SimpleButton from "./components/simpleButton";
 
 const App = ({ state: { count, refreshID } }) => (
   <div className="App">
-    <div>Counts: {count}</div>
+    <div>Count: {count}</div>
     <div>{SimpleButton()}</div>
     <div>Refresh ID: {refreshID}</div>
   </div>
