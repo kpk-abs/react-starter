5d508e6f0a27bc22c32ae76c9fc55e5e555f45f7
kpk-abs <120717722+kpk-abs@users.noreply.github.com>
2023-03-13 11:25:59 +0530

    Test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import { React } from 'react';
-import './App.scss';
-import SimpleButton from './components/simpleButton';
+import { React } from "react";
+import "./App.scss";
+import SimpleButton from "./components/simpleButton";
 
-const App = ({ state: { count, refreshID }}) =>
-	<div className="App">
-		<div>Count: { count }</div>
-		<div>{ SimpleButton() }</div>
-		<div>Refresh ID: { refreshID }</div>
-	</div>;
+const App = ({ state: { count, refreshID } }) => (
+  <div className="App">
+    <div>Counts: {count}</div>
+    <div>{SimpleButton()}</div>
+    <div>Refresh ID: {refreshID}</div>
+  </div>
+);
 
 export default App;
